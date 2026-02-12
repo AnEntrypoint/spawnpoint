0720b7993f7525c88f78cba1942ee04ed830eb59
System <system@local>
2026-02-12 16:31:33 +0200

    work in progress: crouch position adjustment

--- src/sdk/TickHandler.js
+++ src/sdk/TickHandler.js
@@ -44,7 +44,7 @@ export function createTickHandler(deps) {
       }
 
       applyMovement(st, inp, movement, dt)
-      if (inp) physicsIntegration.setCrouch(player.id, !!inp.crouch)
+      if (inp && inp.crouch) physicsIntegration.setCrouch(player.id, !!inp.crouch)
       const wishedVx = st.velocity[0], wishedVz = st.velocity[2]
       const updated = physicsIntegration.updatePlayerPhysics(player.id, st, dt)
       st.position = updated.position
